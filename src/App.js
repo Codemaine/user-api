7b35ee46bd65cd6f0588d2f75ecd65ece21489e4
Jermaine <jermaineantwi22@gmail.com>
2020-11-08 12:17:08 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import { Component } from 'react'
+import { Container, Row, Col, Card } from 'react-bootstrap'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(users => {
+        this.setState({ users: users })
+        console.log(this.state.users)
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <div class="d-flex justify-content-center"><h1>Users</h1></div>
+        <hr />
+        <Container fluid>
+          {this.state.users.map((user) => <Row>
+            <Col>
+              <Card style={{ width: '97vw' }}>
+                <Card.Body>
+                  <Card.Title>{user.name}</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">{user.username}</Card.Subtitle>
+                  <Card.Text>
+                    Email: {user.email}<br />
+                   Address: {user.address.suite}, {user.address.street}, {user.address.city}
+                  </Card.Text>
+                  <Card.Link href={`https://${user.website}`} target="_blank" >{user.website}</Card.Link>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>)}
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
